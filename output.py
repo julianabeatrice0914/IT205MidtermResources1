2324a4cc077280314f21320ae04c471d7bf19c59
Juliana Beatrice Ledesma <154342718+julianabeatrice0914@users.noreply.github.com>
2024-03-05 15:48:33 +0800

    Add files via upload

---
+++ output.py
@@ -0,0 +1,14 @@
+#variable declaration
+x = 20
+message = "Hi po!"
+ratio = 3.14
+
+#checking the type of data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+
+#string formatting
+print(message + " My name is Juliana. I am " + str(x) + " years old.")
+
